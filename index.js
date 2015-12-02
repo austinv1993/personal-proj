1cc3aa35f00b45e2140518ba2c5b6839d2c17956
Austin Vance <austinv_1993@yahoo.com>
2015-12-01 20:31:05 -0700

    starting google auth

--- index.js
+++ index.js
@@ -5,11 +5,32 @@ var express = require('express')
 ,	port = 3000
 ,	mongooseUri = "mongodb://localhost:27017/irrigation-motor-control"
 ,	UserCtrl = require('./controllers/userCtrl.js')
+,	passport = require('passport')
+,	flash = require('connect-flash')
+,	morgan = require('morgan')
+,	cookieParser = require('cookie-parser')
+,	session = require('express-session')
+// ,	configDB = require('./config/database.js')
 ,	app = express()
 
+//mongoose.connect("mongodb://username:password@ds031903.mongolab.com:31903/example");
+app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ 
+	secret: 'anythingbutwhatwasalreadyherelololol',
+	saveUninitialized: true,
+	resave: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
+//require('./routes.js')(app, passport);
 
 //USER//
 app.post('/api/user', UserCtrl.addUser);
@@ -20,13 +41,12 @@ app.delete('/api/user', UserCtrl.deleteUser);
 
 
 
-var a = 4;
 
 
 app.listen(port, function() {
 	console.log("Listening on port:", port);
 });
 mongoose.connect(mongooseUri);
-mongoose.connection.once('open', function() {
-	console.log("Connected to MongoDB at:", mongooseUri);
-});
+// mongoose.connection.once('open', function() {
+// 	console.log("Connected to MongoDB at:", mongooseUri);
+// });
